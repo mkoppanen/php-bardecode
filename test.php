6c5a310db9faf83969016511bd52b29cd5411ba3
Mikko Koppanen <mkoppanen@gameboy.local>
2009-11-12 13:03:53 +0000

    Delete test.php because it is not needed

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-dl("bardecode.so");
-
-$b = new bardecode();
-var_dump($b->setParameter(bardecode::BARCODE_ZONE, array(1, 1, 22, 22)));
-var_dump($b->getParameter(bardecode::BARCODE_ZONE));
-//var_dump($b->read("test.jpg"));
